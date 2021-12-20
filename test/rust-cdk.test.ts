bba75c09de3764d0ae14269ca79038fdf81a1017
Mike McCall <1548024+mmccall10@users.noreply.github.com>
2021-12-20 06:55:03 -0800

    Initial commit

---
+++ test/rust-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RustCdk from '../lib/rust-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/rust-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RustCdk.RustCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
